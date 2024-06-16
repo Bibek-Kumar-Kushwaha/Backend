61e6bd9fb033fc455dc8df002bcb30edc4072212
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-06-16 07:11:39 +0545

    resolve bug

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectToDB from './connectDB/connectDB.js';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectToDB from './connectDB/connectDB.js';
 import userRoute from './routes/userRoute.js';
 
 dotenv.config();
@@ -9,8 +13,6 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +25,6 @@ app.get('/', (req, res) => {
     res.send('Server is ready to serve...');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`);
-});
+
+export default app;
+
