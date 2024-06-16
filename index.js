037cb4e28e04ce0721e600cee6ceda41118418dd
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-06-16 10:55:13 +0545

    vercel dubug

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectToDB from './connectDB/connectDB.js';
+import connectToDB from './connectDB.js';
 import userRoute from './routes/userRoute.js';
 
 dotenv.config();
 const app = express();
 
-//app.use(cors());
+// Enable CORS
 app.use(cors({
-    origin: process.env.FRONTENDED_URL, // Allow requests from your React frontend
+    origin: 'https://portfolio-github-io-cyan.vercel.app/', // Replace with your frontend URL
     methods: ["GET", "PUT", "DELETE", "POST"],
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true,
 }));
+
 const PORT = process.env.PORT || 3000;
 
+// Other middleware and routes setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
 app.use('/', userRoute);
 
 app.get('/', (req, res) => {
     res.send('Server is ready to serve...');
 });
 
+// Connect to MongoDB
 connectToDB();
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
