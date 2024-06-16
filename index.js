1eb9a038b648cf419f7fe60173eb665123c33c59
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-06-16 07:50:21 +0545

    edit

--- index.js
+++ index.js
@@ -7,27 +7,31 @@ import userRoute from './routes/userRoute.js';
 dotenv.config();
 const app = express();
 
-//app.use(cors());
-app.use(cors({
-    origin: process.env.FRONTENDED_URL, // Allow requests from your React frontend
-    methods: ["GET", "PUT", "DELETE", "POST"],
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-}));
-
 const PORT = process.env.PORT || 3000;
 
+// CORS configuration
+app.use(cors({
+    origin: process.env.FRONTENDED_URL, // Replace with your frontend URL
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+}));
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-app.use('/', userRoute);
-
+// Routes
 app.get('/', (req, res) => {
     res.send('Server is ready to serve...');
 });
+app.use('/', userRoute);
 
+// Connect to MongoDB
 connectToDB();
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
+
+export default app;
