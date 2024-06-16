a52cdb9543de894ff6f06cd6e61658d4574c2cb8
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-06-16 07:39:09 +0545

    bugs resolve

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ import connectToDB from './connectDB/connectDB.js';
 import userRoute from './routes/userRoute.js';
 
 dotenv.config();
-
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTENDED_URL, // Allow requests from your React frontend
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectToDB();
+
 
 app.use('/', userRoute);
 
@@ -23,6 +27,7 @@ app.get('/', (req, res) => {
     res.send('Server is ready to serve...');
 });
 
+connectToDB();
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
