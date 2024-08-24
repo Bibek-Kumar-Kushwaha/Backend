630a36303facb89e20f48f0178ea162907cbdb68
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-08-24 13:57:04 +0545

    update package

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import helmet from 'helmet'
 import connectToDB from './connectDB/connectDB.js';
 import userRoute from './routes/userRoute.js';
-import visitRoute from './routes/visitRoute.js';
+
 dotenv.config();
 const app = express();
 
-//app.use(cors());
+// app.use(cors());
 app.use(cors({
-    origin: [
-        "https://www.bibekkumarkushwaha.com.np",
-        "http://localhost:5173"
-    ],
+    origin: "https://www.bibekkumarkushwaha.com.np",
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true, 
 }));
+app.use(helmet());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', userRoute);
-app.use('/api', visitRoute);
 
 app.get('/', (req, res) => {
     res.send('Server is ready to serve...');
